e0e7fe32dd7176492f41c3aaed6c84a0921816cf
vaishnav <vaishnavcveez@gmail.com>
2020-08-27 17:19:54 +0530

    trained model saved as a file using pickle, and reload it in to linear

--- test.py
+++ test.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import sklearn
 from sklearn import linear_model
+import matplotlib.pyplot as pyplot
+from matplotlib import style
+import pickle
 from sklearn.utils import shuffle
 
 data = pd.read_csv("student-mat.csv", sep=";")
@@ -15,13 +18,19 @@ y = np.array(data[predict])
 
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
-linear = linear_model.LinearRegression()
+'''linear = linear_model.LinearRegression()
 linear.fit(x_train, y_train)
-
 acc = linear.score(x_test, y_test)
 print(acc)
+
+with open('studentmodel.pickle', 'wb') as f:
+    pickle.dump(linear, f)'''
+
+pickle_in = open('studentmodel.pickle', 'rb')
+linear = pickle.load(pickle_in)
+
 print("Coefficient:\n", linear.coef_)
-print('intercept: \n',linear.intercept_)
+print('intercept: \n', linear.intercept_)
 
 prediction = linear.predict(x_test)
 for x in range(len(prediction)):
