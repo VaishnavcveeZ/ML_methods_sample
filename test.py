5fb31ecc2073bdaf62102473462904af3100e5f7
vaishnav <vaishnavcveez@gmail.com>
2020-08-27 16:02:58 +0530

    liniear regression model based prediction

---
+++ test.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import linear_model
+from sklearn.utils import shuffle
+
+data = pd.read_csv("student-mat.csv", sep=";")
+
+data = data[['G1', 'G2', 'G3', 'studytime', 'failures', 'absences']]
+
+predict = 'G3'
+
+x = np.array(data.drop([predict], 1))
+y = np.array(data[predict])
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+
+linear = linear_model.LinearRegression()
+linear.fit(x_train, y_train)
+
+acc = linear.score(x_test, y_test)
+print(acc)
+print("Coefficient:\n", linear.coef_)
+print('intercept: \n',linear.intercept_)
+
+prediction = linear.predict(x_test)
+for x in range(len(prediction)):
+    print(prediction[x], x_test[x], y_test[x])
