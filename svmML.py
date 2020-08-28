a0094df9999ec7db14852970e3e606761acb5f9c
vaishnav <vaishnavcveez@gmail.com>
2020-08-28 19:00:14 +0530

    'updates'

---
+++ svmML.py
@@ -0,0 +1,30 @@
+import sklearn
+from sklearn import datasets
+from sklearn import svm
+from sklearn import metrics
+from sklearn.neighbors import KNeighborsClassifier
+
+cancer = datasets.load_breast_cancer()
+
+#print(cancer.feature_names)
+#print(cancer.target_names)
+x=cancer.data
+y=cancer.target
+x_train,x_test,y_train, y_test = sklearn.model_selection.train_test_split(x,y, test_size=0.2)
+#print(x_train,y_train)
+
+classes = ['malignant' 'benign']
+
+clf = svm.SVC(kernel='linear', C=1)
+kn=KNeighborsClassifier(n_neighbors=7)
+
+clf.fit(x_train,y_train)
+kn.fit(x_train,y_train)
+
+y_predt1= clf.predict(x_test)
+y_predt2= kn.predict(x_test)
+
+acc = metrics.accuracy_score(y_test,y_predt1)
+acck = metrics.accuracy_score(y_test,y_predt2)
+print('svm = ',acc)
+print('KNN = ',acck)
